dfd2368641cb98906e227854f7969b41f2f8137d
Tysonic <tysonic@Windows.Tysonic>
2020-06-06 16:01:52 +0300

    updating clients view

--- zuku/Views/ClientsViews.py
+++ zuku/Views/ClientsViews.py
@@ -11,9 +11,9 @@ def addClients():
 
     form = request.json
     if form.validate_on_submit():
-        new_client = Clients(username = form.username,fname = form.fname,oname = form.oname,email =form.email,
-                             tel = form.tel,apart_no = form.apart_no, floor =form.floor,estate =form.estate,address = form.address,
-                             city = form.city)
+        Clients.query.filter_by(username=form['username']).update(dict(fname = form['fname'],oname = form['oname'],
+                             tel = form['tel'],apart_no = form['apart_no'], floor =form['floor'],estate =form['estate'],address = form['address'],
+                             city = form['city']))
         db.session.add(new_client)
         db.session.commit()
         return redirect(url_for('clients.listClients'))
@@ -26,3 +26,7 @@ def addClients():
 @client_blueprint.route('/list clients', methods=['POST','GET'])
 def listClients():
     return render_template('clientsList.html', clients = Clients.query.all())
+
+@client_blueprint.route('/list client api')
+def listClientApi():
+    return  {"clients":Clients.query.all()}
